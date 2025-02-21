bf5ad6f99b3b8fb7462378db773425b1dcc42587
Rashmi Ranjan Nayak <rrnayak165@gmail.com>
2025-02-21 11:02:25 +0530

    remove : unused imports

--- src/pages/employee/CreateEmployee.jsx
+++ src/pages/employee/CreateEmployee.jsx
@@ -1,14 +1,5 @@
-import { Link, useNavigate } from "react-router-dom";
-import styles from "../../css/CreateEmployeeForm.module.css";
-import "../../css/Base.css";
-import { useEffect, useRef, useState } from "react";
-import { toast } from "react-toastify";
-import { getDepartments } from "../../services/DepartmentService";
-import { addEmployee } from "../../services/EmployeeService";
-import { addDepartmentToEmployee } from "../../services/RelationShipService";
 import EmployeeRegisterForm from "../../components/EmployeeRegisterForm";
 
-const BASE_URL = "http://localhost:2200/api/v1";
 export default () => {
   return <EmployeeRegisterForm empId={undefined} type={"Add"} />;
 };
--- src/pages/employee/EditEmployee.jsx
+++ src/pages/employee/EditEmployee.jsx
@@ -1,20 +1,8 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
-import styles from "../../css/CreateEmployeeForm.module.css";
-import { useEffect, useState } from "react";
-import Select from "react-select/base";
-import { toast } from "react-toastify";
-import { getDepartments } from "../../services/DepartmentService";
-import {
-  addDepartmentsToEmployee,
-  getEmployeeWithDepartments,
-} from "../../services/RelationShipService";
-import { updateEmployee } from "../../services/EmployeeService";
+import { useParams } from "react-router-dom";
 import EmployeeRegisterForm from "../../components/EmployeeRegisterForm";
 
 export default () => {
   const { empId } = useParams();
 
-  return (
-    <EmployeeRegisterForm empId={empId} type={"Update"}/>
-  );
+  return <EmployeeRegisterForm empId={empId} type={"Update"} />;
 };
